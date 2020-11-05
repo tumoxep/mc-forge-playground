47ffb0e91bab0caae8ec5bdb3bc24bc34ba70b17
Тимофей Трофимов <t.trofimov@just-work.org>
2020-11-06 01:49:34 +0300

    getTotalWorldTime check

--- src/main/java/com/example/examplemod/MyForgeEventHandler.java
+++ src/main/java/com/example/examplemod/MyForgeEventHandler.java
@@ -2,12 +2,25 @@ package com.example.examplemod;
 
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.WorldTickEvent;
 import net.minecraftforge.event.world.WorldEvent.Load;
 
 @EventBusSubscriber
 public class MyForgeEventHandler {
+	public long totalWorldTime = 0;
+	
     @SubscribeEvent
     public void worldLoad(Load event) {
         System.out.println("Hello world");
+    }
+
+    @SubscribeEvent
+    public void worldTick(WorldTickEvent event) {
+    	long tickCheckWorldTime = event.world.getTotalWorldTime();
+    	
+    	if (totalWorldTime != tickCheckWorldTime) {
+    		totalWorldTime = tickCheckWorldTime;
+    		System.out.println("time: " + totalWorldTime);
+    	}
     }
 }
